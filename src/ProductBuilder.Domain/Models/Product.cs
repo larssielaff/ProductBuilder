8205cef423ce9d9c75f013b723e84ac2dc81d1b1
Stefan Merkel <merkel.s.ulm@gmail.com>
2017-12-07 15:23:21 +0100

    update

--- src/ProductBuilder.Domain/Models/Product.cs
+++ src/ProductBuilder.Domain/Models/Product.cs
@@ -3,10 +3,15 @@
     using Asd.Domain.Core.Models;
     using System;
     using System.Collections.Generic;
+
     public class Product : AsdEntity
     {
         public string Title { get; set; }
+
         public string ProductVision { get; set; }
+
+        public virtual ICollection<UserRole> UserRoles { get; set; }
+
         public virtual ICollection<Team> Teams { get; set; }
 
         public Product(Guid id) 
@@ -19,6 +24,7 @@
 
         protected Product()
         {
+            UserRoles = new HashSet<UserRole>();
             Teams = new HashSet<Team>();
         }
     }
