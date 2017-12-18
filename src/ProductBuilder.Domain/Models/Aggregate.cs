e79fc990ae7a670a599e6df2b4f423ce0c5e3b75
Stefan Merkel <merkel.s.ulm@gmail.com>
2017-12-18 21:25:27 +0100

    update

--- src/ProductBuilder.Domain/Models/Aggregate.cs
+++ src/ProductBuilder.Domain/Models/Aggregate.cs
@@ -3,7 +3,6 @@
     using Asd.Domain.Core.Models;
     using System;
     using System.Collections.Generic;
-
     public class Aggregate : AsdEntity
     {
         public Guid? ProductId { get; set; }
@@ -14,14 +13,21 @@
 
         public string Name { get; set; }
 
-        public Aggregate(Guid id) 
-            : this()
+        public virtual ICollection<AggregateProperty> AggregateProperties { get; set; }
+
+        public virtual ICollection<AggregateProperty> LinkedAggregateProperties { get; set; }
+
+        public Aggregate(Guid id) : this()
         {
             if (id == Guid.Empty)
                 throw new ArgumentNullException(nameof(id));
             Id = id;
         }
 
-        protected Aggregate() { }
+        protected Aggregate()
+        {
+            AggregateProperties = new HashSet<AggregateProperty>();
+            LinkedAggregateProperties = new HashSet<AggregateProperty>();
+        }
     }
 }
